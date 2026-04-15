448737bf385020148cc1d938daea9b1d0ba164ae
sowjanyanagraj757-jpg <sowjanyanagraj757@gmail.com>
2026-04-15 00:58:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from transformers import pipeline
 @st.cache_resource
 def load_summsrizer();
   return pipeline("summerization",model="sshleifer/distilbart-can-12-6")
+ 
+  
+  summarizer=load_summarizer()
+  st.title("ai text summarizer")
+  st.write("enter a long text below,and get a concise summary!")
+  long_text=st.text_area("enter text to summarize:",height=200)
+  
